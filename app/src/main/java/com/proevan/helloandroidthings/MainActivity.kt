b98cce534d0c02d5157cf5ecd1a9c651ccb59015
Evan Lin <yanghsin.lin@gmail.com>
2017-07-30 05:59:18 +0800

    remove default android things hints in mainActivty

--- app/src/main/java/com/proevan/helloandroidthings/MainActivity.kt
+++ app/src/main/java/com/proevan/helloandroidthings/MainActivity.kt
@@ -6,26 +6,6 @@ import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManagerService
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * val service = PeripheralManagerService()
- * val mLedGpio = service.openGpio("BCM6")
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * mLedGpio.value = true
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- *
- */
 class MainActivity : Activity() {
 
     companion object {
